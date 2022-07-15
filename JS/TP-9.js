6094f7d3b336ee981c06e3256ae245ea1e41e500
maxianzo <104526106+Maxianzo@users.noreply.github.com>
2022-07-15 16:55:53 -0300

    Add files via upload

---
+++ JS/TP-9.js
@@ -0,0 +1,11 @@
+function nightmode(){
+document.body.classList.toggle("dark");
+var encabezado=document.getElementById("encabezado");
+encabezado.classList.toggle("dark-header");
+}
+
+
+function info(name){
+   }
+var name=prompt("bienvenido:¿Cual es su nombre?")
+ alert("Hola " +name+ " espero que encuetre los mejores productos , sino no dudes en contactarnos");
